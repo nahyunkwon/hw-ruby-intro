bc84cbe6f7f7a84d8c3e37c89320ec6eb11153f1
Nahyun Kwon <skgus2624@gmail.com>
2022-02-16 23:48:23 -0600

    resolve error

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -6,7 +6,7 @@ def sum arr
   # YOUR CODE HERE
   sum = 0
   for i in 0..arr.length()
-    sum += i
+    sum += arr[i].to_i
   end
   return sum
 end
@@ -14,20 +14,20 @@ end
 def max_2_sum arr
   # YOUR CODE HERE
   result = 0
-  
+
   if arr.length() == 0
     return 0
   elsif arr.length() == 1
     return arr.last
   end
-  
+
   arr = arr.sort
   result = result + arr.last
   arr.pop
   result = result + arr.last
 
   return result
-  
+
 end
 
 def sum_to_n? arr, n
@@ -35,10 +35,9 @@ def sum_to_n? arr, n
   if arr.length() == 0
     return false
   end
-  
+
   for i in 0...arr.length() - 1
     for j in i+1...arr.length()
-      puts i,j
       if arr[i] + arr[j] == n
         return true
       end
@@ -56,25 +55,33 @@ end
 
 def starts_with_consonant? s
   # YOUR CODE HERE
-  if ['A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u'].include? s[0]
+  if s[0] =~ /[AEIOUaeiou]/ or s.length == 0
+    return false
+  end
+
+  if s[0]=~ /[a-zA-Z]/
     return true
   else
     return false
   end
+
 end
 
 def binary_multiple_of_4? s
   # YOUR CODE HERE
+  if s == ''
+    return false
+  end
   for i in 0...s.length()
     if !['0', '1'].include? s[i]
       return false
     end
   end
-  
+
   binary = s.to_i(2)
- 
+
   return binary%4 == 0
-  
+
 end
 
 # Part 3
@@ -91,26 +98,27 @@ class BookInStock
       raise ArgumentError
     end
 
-    if price.is_a? Float and price > 0
+    if price > 0
       @price = price
     else
       raise ArgumentError
     end
   end
 
+  attr_accessor :isbn, :price
+
   def price_as_string
-    return '$' + @price.round(2).to_s
+    return '$' + ('%.2f' % @price).to_s
   end
-  
+
 end
 
-#puts sum([1,2,3])
+#puts sum([-1,2,3])
 #puts hello('Nahyun')
-#puts starts_with_consonant?('ai')
+#puts starts_with_consonant?('#')
 #puts max_2_sum([2,2])
 #puts sum_to_n?([1,2,1], 2)
 #puts binary_multiple_of_4?('11001')
 
-book =  BookInStock.new("1111", 20.288)
-
-puts book.price_as_string
+#book =  BookInStock.new("20", 20)
+#puts book.price_as_string
